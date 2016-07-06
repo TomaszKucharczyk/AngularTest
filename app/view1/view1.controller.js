c7ec3ddbd98ea9558f7fd95a685e9b9ecabab9c9
TomaszKucharczyk <tomaszkucharczyk7@gmail.com>
2016-07-06 14:14:03 +0200

    No i kij

--- app/view1/view1.controller.js
+++ app/view1/view1.controller.js
@@ -6,7 +6,7 @@ angular.module('myApp.view1').
         controller: ['$http', '$routeParams', 
             function ViewController($http, $routeParams) {
                 
-                this.text = '';
+                this.text = ' ';
                 
                 this.change = function(text)
                 {
@@ -18,16 +18,26 @@ angular.module('myApp.view1').
                 this.change2 = function(text)
                 {
                     var table = [...text];
+                    var tmp = [table.length];
                     
                     for (var i = 0; i < table.length; i++) {
                         
-                        var tmp = table[i].charCodeAt(0);
-                        tmp + 10;
-                        console.log(tmp);
-                        var string = String.fromCharCode(tmp);
-                        table[i] = string;
+                        tmp[i] = table[i].charCodeAt(0);
+                        tmp[i] = tmp[i] + 10;
+//                        var tmp = table[i].charCodeAt(0);
+//                        tmp + 10;
+//                        console.log(tmp);
+//                        var string = String.fromCharCode(tmp);
+//                        table[i] = string;
                     }
+                    var string = String.fromCharCode(tmp);
                     return table;
+//                        var tmp = [table.length];
+//                        for (var i = 0; i < table.length; i++) {
+//
+//                            tmp[i]=table[(table.length - 1)-i];
+//                        }
+//                        return tmp;
                 };
             }
         ]
